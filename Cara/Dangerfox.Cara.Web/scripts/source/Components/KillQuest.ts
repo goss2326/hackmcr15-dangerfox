56f124b5216d20730a7fcc00ff917e0ce3417b62
dangerworm <dangerworm@gmail.com>
2015-10-25 00:24:47 +0100

    Added quest classes

---
+++ Cara/Dangerfox.Cara.Web/scripts/source/Components/KillQuest.ts
@@ -0,0 +1,13 @@
+﻿module Dangerfox.Cara.Components
+{
+    export class KillQuest extends Quest
+    {
+        public targetId: number;
+        public amount: number;
+
+        constructor(game: Phaser.Game)
+        {
+            super(game);
+        }
+    }
+}
